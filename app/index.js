4ce3a1af0f6ef288a8c91f1955ee6c58c6466f0a
Adrij Shikhar <adrijshikhar85@gmail.com>
2020-09-04 11:44:01 +0530

    Changes mongodb port

--- app/index.js
+++ app/index.js
@@ -9,7 +9,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Connect to MongoDB
-mongoose.connect('mongodb://mongo:27017/app',
+mongoose.connect('mongodb://mongo:9000/app',
   // mongo config when working with docker
   { useNewUrlParser: true }
 )
